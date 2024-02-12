08815c7fc201e0ebd12090f7f0511f191e3d6926
Jhoanna May Lacorte <152822639+JhoannaMay@users.noreply.github.com>
2024-02-12 02:31:37 +0000

    Updated Student

--- Student.java
+++ Student.java
@@ -2,5 +2,23 @@ public class Student{
     private String studentNumber;
     private String lastName;
     private String firstName;
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
     
 }
